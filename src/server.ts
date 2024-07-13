5c8726eab7953a33fb8f3c98cc783f63046c4944
Douglas José Barboza <douglasbarbozajm@gmail.com>
2024-07-13 18:52:03 -0300

    2 class finished

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,16 @@
-import fastify from "fastify";
 import cors from "@fastify/cors";
-import { createTrip } from "./routes/create-trips";
-import { confirmTrip } from "./routes/confirm-trip";
+import fastify from "fastify";
 import {
   serializerCompiler,
   validatorCompiler,
 } from "fastify-type-provider-zod";
+import { confirmParticipant } from "./routes/confirm-participant";
+import { confirmTrip } from "./routes/confirm-trip";
+import { createActivity } from "./routes/create-activity";
+import { createLink } from "./routes/create-link";
+import { createTrip } from "./routes/create-trips";
+import { getActivities } from "./routes/get-activities";
+import { getLinks } from "./routes/get-links";
 
 const app = fastify();
 app.register(cors, {
@@ -17,6 +22,11 @@ app.setSerializerCompiler(serializerCompiler);
 
 app.register(createTrip);
 app.register(confirmTrip);
+app.register(confirmParticipant);
+app.register(createActivity);
+app.register(getActivities);
+app.register(createLink);
+app.register(getLinks);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("Server running!");
